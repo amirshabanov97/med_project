528bad2f9534dcdec60a2e49de08c64ffbc68633
amirshabanov97 <amir.shabanov@is.sdu.edu.kz>
2017-01-15 16:38:22 +0600

    animation completely upgraded

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	var windowWidth = window.Width;
-	var windowHeight = window.Height;
+	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
 	
 	var card_container = $('.card_container');
 	var card_container_row = Math.ceil(card_container.children().length / 2);
@@ -9,14 +9,15 @@ $(document).ready(function() {
 	var start_to_card = $('[data-name="base"]');
 
 	card_container.width(windowWidth * 2);
-	card_container.Height(windowHeight * card_container_row);
+	card_container.height(windowHeight * card_container_row);
 
 	$('.card').width(windowWidth);
 	$('.card').height(windowHeight);
+	console.log("window size : " + windowWidth + ' X '  + windowHeight);
+	console.log('container row : ' + card_container_row);
+	console.log('container size : ' + card_container.width() + ' X ' + card_container.height());
 
 	animateToCard(start_from_card, start_to_card, 0, 0);
-
-
 });
 
 $('[data-to]').on('click', function(event) {
@@ -25,37 +26,39 @@ $('[data-to]').on('click', function(event) {
 
 	var fromDirection = $("[data-name='"+fromCard+"']");
 	var toDirection = $("[data-name='"+toCard+"']");
+
+	animateToCard(fromDirection, toDirection, 1000, 2000);
 })
 
 
-function animateToCard(cardnumber, body, scaleSpeed, transferSpeed) {
-	card = "#card" + cardnumber;
-	var x = $(card).offset().left;
-	var y = $(card).offset().top;
+function animateToCard(fromDirection, toDirection, scaleSpeed, scrollSpeed) {
+	var x = $(toDirection).offset().left;
+	var y = $(toDirection).offset().top;
 	
+	console.log('position card : ' + x + 'X' + y);
 
-	body.velocity({
+	$(fromDirection).velocity({
 		scale: 1,
 	}, {
 		duration: scaleSpeed,
 		queue: false,
 		complete: function() {
-			body.animate({
+			$('body').animate({
 				scrollLeft: x,
-				scrollTop: y,
+				scrollTop: y
 			}, {
-				duration: transferSpeed,
-				easing: 'easeOutExpo',
+				duration: scrollSpeed,
+				easing: 'easeOutQuint',
 				queue: false,
 				complete: function() {
-					body.velocity({
-						scale: 1.2,
+					$(toDirection).velocity({
+						scale: 1.24,
 					}, {
 						duration: scaleSpeed,
 						queue: false,
 					})
 				}
 			});
-		}
-	});
-};
+			}
+		});	
+	};
