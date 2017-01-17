443594485dd99334682b9ea31723d0082571188d
d1vin3 <temirlan.nurmakhanov@is.sdu.edu.kz>
2017-01-18 03:44:47 +0600

    interactive human_body

--- app/js/client/doctoroncall/doctoroncall.js
+++ app/js/client/doctoroncall/doctoroncall.js
@@ -18,7 +18,10 @@ angular.module("clientApp").directive('jqdatepicker', function() {
 });
 
 angular.module("clientApp").controller('doctoroncall', ['$scope', function($scope) {
+	
+	$scope.tabState = "whom";
 	$scope.whom_init = true;
+	$scope.symptoms = [];
 
 	$scope.head_parts = [
 	{
@@ -49,28 +52,38 @@ angular.module("clientApp").controller('doctoroncall', ['$scope', function($scop
 		name: 'face',
 		img_src: 'img/icons/organs/face.png'
 	},
-	]//################################NEED TO ADD SCROLLING #####################################################
+	];
+	//################################NEED TO ADD SCROLLING #####################################################
 
 	$scope.head_symptoms = [
 	{
+		id: 0,
 		name: 'Боль в висках'
 	},
 	{
+		id: 1,
 		name: 'Головокружение'
 	},
 	{
+		id: 2,
 		name: 'Я не могу описать боль'
 	},
 	{
+		id: 3,
 		name: 'Боль в затылке'
 	},
 	{
+		id: 4,
 		name: 'Давление'
 	},
 	{
+		id: 5,
 		name: 'Боль в затылке'
 	},
 	]
+
+	$scope.selected_symptoms = [];
+	
 
 	$scope.changeTabState = function(state) {
 		$scope.tabState = state;
@@ -89,11 +102,25 @@ angular.module("clientApp").controller('doctoroncall', ['$scope', function($scop
 		$scope.whom_select_area = false;
 		$scope.whom_head_parts = true;
 	}
-	$scope.select_symptom = function(name, img_src){
+	$scope.select_symptom = function(name, img_src) {
 		$scope.selected_name = name;
 		$scope.selected_img = img_src;
 		$scope.whom_head_parts =false;
 		$scope.whom_select_symptoms = true;
+		if(name=="head"){
+			$scope.symptoms = $scope.head_symptoms;
+		}
+	}
+	$scope.add_symptom = function(name) {
+		if (!$scope.selected_symptoms.includes(name)){
+			$scope.selected_symptoms.push(name);
+		}
+	}
+
+	$scope.prepare_request = function(){
+		$scope.whom_select_symptoms = false;
+		$scope.whom_selected_symptom = true;
 	}
 
 }]);
+		
