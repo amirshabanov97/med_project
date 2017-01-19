b20325acc581fded448b61350e88fa40358ed5d0
amirshabanov97 <amir.shabanov@is.sdu.edu.kz>
2017-01-19 17:41:35 +0600

    changed pord of medicine

--- medicine.js
+++ medicine.js
@@ -685,8 +685,7 @@ app.get("/api/doctor/requests_list", function(req, res) {
 	});
 });
 
-
-server.listen(6500, '192.168.1.110', function() {
+server.listen(7000, '192.168.1.110', function() {
 	console.log('Listnening on ' + server.address().port);
 	console.log('Showing on ' + server.address().address);
 });
